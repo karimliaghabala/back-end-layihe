d2353c52cc78b2f5f8d8a5763f2424d5af7a5c70
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-14 01:24:26 +0400

    Update home.jsx

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import Coins from '../Coins/coins'
 import '../Search-result/search-result.css'
 import './home.css';
@@ -12,6 +12,8 @@ const Home = () => {
     const [coin, setCoin] = useState(true)
     const [searchr, setSearchr] = useState(false)
     const [data, setData] = useState([])
+    const [sData, setSData] = useState([])
+    const inputRef = useRef(null);
 
     const onClickButton = (e) => {
         e.preventDefault()
@@ -43,6 +45,15 @@ const Home = () => {
             .then(res => res.json())
             .then(apiData => setData(apiData))
     }, [])
+    useEffect(() => {
+        const url = 'https://excample-api.vercel.app/search'
+        const query = encodeURIComponent(inputRef.value)
+        fetch(`${url}?q=${query}`)
+            .then(res => res.json())
+            .then(apiSData => setSData(apiSData))
+    }, [])
+    console.log(sData)
+    console.log(inputRef.value)
     return (
         <div className='container'>
             <form action="">
@@ -50,7 +61,7 @@ const Home = () => {
                     <h1 onClick={falseData}>HOMEPAGE</h1>
                     <p>Input field</p>
                     <div className="input-search">
-                        <input type="text"/>
+                        <input type="text" ref={inputRef} />
                         <button onClick={onClickButton} >Search</button>
                     </div>
                     <div className="filter">

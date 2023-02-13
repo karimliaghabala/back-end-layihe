28d3042637559f76614f3814f386cbb134b3711e
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-13 04:26:58 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/home'
 import Info from './components/Coins-info/info'
-import SearchResult from './components/Search-result/search-result'
 import Ccoins from './components/Commemorative_coins/cCoins';
 import Ecoins from './components/Exclusive_coins/eCoins';
 import Bcoins from './components/Bullion_coins/bCoins';
@@ -15,7 +14,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/info' element={<Info />} />
-        <Route path='/searchresult' element={<SearchResult />} />
         <Route path='/ccoins' element={<Ccoins />} />
         <Route path='/ecoins' element={<Ecoins />} />
         <Route path='/bcoins' element={<Bcoins />} />
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Coins from '../Coins/coins'
-import Search_result from '../Search-result/search-result'
+import '../Search-result/search-result.css'
 import './home.css';
 import Svg1 from '../../assets/chevron-up.svg'
 import Svg2 from '../../assets/chevron-down.svg'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
-
     const [svg1, setSvg1] = useState(false)
     const [form, setForm] = useState(false)
     const [coin, setCoin] = useState(true)
     const [searchr, setSearchr] = useState(false)
-
+    const [data, setData] = useState([])
 
     const onClickButton = (e) => {
         e.preventDefault()
@@ -20,7 +20,6 @@ const Home = () => {
         setSearchr(true)
         setCoin(false)
     }
-
     const onClickSvg = () => {
         setSvg1(true)
         setForm(true)
@@ -39,7 +38,6 @@ const Home = () => {
         setSearchr(false)
         setCoin(true)
     }
-    const [data, setData] = useState([])
     useEffect(() => {
         fetch('https://excample-api.vercel.app/')
             .then(res => res.json())
@@ -52,8 +50,8 @@ const Home = () => {
                     <h1 onClick={falseData}>HOMEPAGE</h1>
                     <p>Input field</p>
                     <div className="input-search">
-                        <input type="text" />
-                        <button onClick={onClickButton}>Search</button>
+                        <input type="text"/>
+                        <button onClick={onClickButton} >Search</button>
                     </div>
                     <div className="filter">
                         <p>Advanced filter</p>
@@ -86,16 +84,16 @@ const Home = () => {
                     </div>
                     <div className="right-f">
                         <div>
-                            <p>Price</p>            
-                                <select>
+                            <p>Price</p>
+                            <select>
                                 {data?.map(item => (
                                     <option>{item.price}</option>
                                 ))}
                             </select>
                         </div>
                         <div>
-                            <p>Year of issue</p>                
-                                <select>
+                            <p>Year of issue</p>
+                            <select>
                                 {data?.map(item => (
                                     <option>{item.year}</option>
                                 ))}
@@ -104,7 +102,26 @@ const Home = () => {
                     </div>
                 </div> : null}
             </form>
-            {searchr ? <Search_result /> : null}
+            {searchr ? <div>
+                <h1>Coins</h1>
+                <div className='container-c'>
+                    <div className="left-rows">
+                        <ul>
+                            {data?.map((post) => (
+                                <li key={post.id}>
+                                    <div className="coin-row" >
+                                        <div><img className="img-c" src={post.link1} alt="img" /></div>
+                                        <div>
+                                            <Link to="/info">{post.texth1}</Link>
+                                            <p>{post.textshort}</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div> : null}
             {coin ? <Coins /> : null}
         </div>
     )
